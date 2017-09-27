4e6de87daaa82e816340f9cf6b81ea4304773a63
Nipesh Roy <roy.nip@husky.neu.edu>
2017-09-27 00:20:55 -0400

    Added test for epoll - its working incorrectly
    Signed-off-by : Nipesh Roy <roy.nip@husky.neu.edu>

--- test/echo.c
+++ test/echo.c
@@ -9,12 +9,12 @@
 
 void fd_handle(sme_mech_t *m, sme_fd_t *fde, void*data)
 {
-    //char buf[100];
-	float f = 0;
-    read(fde->fd, &f, sizeof(float));
-    //buf[5] = '\0';
-    //printf("Received data : %s\n", buf);
-    printf("Received data : %0.2f\n", f);
+    char buf[100];
+	//float f = 0;
+    //read(fde->fd, &f, sizeof(float));
+    read(fde->fd, buf, 5);
+    printf("Received data : %s\n", buf);
+    //printf("Received data : %0.2f\n", f);
 }
 
 void pid_handle(sme_mech_t *mech, sme_proc_t *proce, void*data)
@@ -24,48 +24,50 @@ void pid_handle(sme_mech_t *mech, sme_proc_t *proce, void*data)
 
 int main()
 {
-    int pid, *fd = NULL;
+    int pid;// *fd = NULL;
     int p_fd[2];
-    //int ret;
-	char *arg[6];
+    int ret;
+	int i = 0;
+	//char *arg[6];
     sme_mech_t *m = NULL;
     m = epoll_mech_init();
     //create pipe
-    pipe2(p_fd, O_NONBLOCK);
 
-    epoll_mech_add_fd(m, p_fd[0], SME_READ, fd_handle, NULL);
+    //epoll_mech_add_fd(m, p_fd[0], SME_READ, fd_handle, NULL);
 
-	arg[0] = "./worker";
+	/*arg[0] = "./worker";
 	arg[1] = "-x";
 	arg[2] = "2";
 	arg[3] = "-n";
 	arg[4] = "3";
-	arg[5] = NULL;
+	arg[5] = NULL;*/
 
-    pid = fork();
-    if(pid<0) {
-        printf("fork failed");
-    }
-    else if (pid == 0) {
-		close(p_fd[0]);
-		dup2(p_fd[1], 1);
-        printf("In child writing on fd : %d\n", p_fd[1]);
-		execv("../worker", arg);
-		/*ret = write(p_fd[1], "hello", 5);
-        if (ret < 0)
-            printf("write failed");
-        _exit(EXIT_SUCCESS);*/
-    }
-    else {
-        printf("In parent\n");
-		close(p_fd[1]);
-		fd = calloc(1, sizeof(int));
-		*fd = p_fd[0];
-		//select_mech_add_proc(m, pid, 0, pid_handle, fd);
-        epoll_mech_add_fd(m, *fd, 0, fd_handle, 0);
-    }
+	do {
+		pipe2(p_fd, O_NONBLOCK);
+
+		pid = fork();
+		if(pid < 0) {
+			printf("fork failed");
+		}
+		else if (pid == 0) {
+			close(p_fd[0]);
+			dup2(p_fd[1], 1);
+			//printf("In child writing on fd : %d\n", p_fd[1]);
+			//execv(".", arg);
+			ret = write(p_fd[1], "hello", 5);
+			if (ret < 0)
+				write(p_fd[1], "write failed", 10);
+			_exit(EXIT_SUCCESS);
+		}
+		else {
+			//printf("In parent\n");
+			close(p_fd[1]);
+			//select_mech_add_proc(m, pid, 0, pid_handle, fd);
+			epoll_mech_add_fd(m, p_fd[0], 0, fd_handle, 0);
+		}
+		i++;
+	}while (i != 5);
 
     epoll_mech_loop_wait(m);
     return 0;
-}
-    
+} 
