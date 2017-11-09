81ac9c022219b4a457b52cb4a5d61b946e3556ff
Nipesh Roy <roy.nip@husky.neu.edu>
2017-11-09 17:04:58 -0500

    Regime 1 code implemented to find cache size
    Signed-off-by : Nipesh Roy <roy.nip@husky.neu.edu>

--- hw5/hw5.c
+++ hw5/hw5.c
@@ -1,47 +1,54 @@
 #include <stdio.h>
 #include <time.h>
-#include <math.h>
 #include <stdlib.h>
+#include <math.h>
 
 /* Considering the cache size is not greater than 8MB */
 int main() {
-    int sum = 0;
-    int i = 0, s = 0;
-    double timeTaken;
-    clock_t start;
-    long int N = 0;
-    int power_two = 12;
-    int *arr = NULL;
-    long int strides = 0;
-    FILE *fp = NULL;
-    
-    fp = fopen("output.txt", "w+");
-    if (fp == NULL) {
-    	printf("\n Unable to open file output.txt");
-    	exit(1);
+	long int sum = 0;
+	int i = 0, s = 0, k = 0;
+	double timeTaken;
+	clock_t start;
+	char *arr = NULL;
+	long int N = 0;
+	long int strides = 0;
+	int power_two = 12;
+	FILE *fp = NULL;
+
+	fp = fopen("output", "w");
+	if (fp == NULL) {
+		printf("Unable to open file output.txt\n");
+		exit(1);
 	}
 
 	/* Regime 1 */
-    for (i = 0 ; i < 12; i++) {
-    	power_two += i;
-    	N = 1 << power_two;
-		fprintf(fp, "Graph plot for %ldKB", (N/1024));
-		arr = (int*) malloc (N);
-		if (arr == NULL) {
-			printf("\nUnable to allocate memory");
-			exit(1);
+	for (k = 0; k < 10; k++)
+	{
+		N = pow(2, power_two);
+		strides = 0;
+		arr = NULL;
+		fprintf(fp, "\nPlot : %d\n", k+1);
+		for (i = 0 ; i < 11; i++) {
+			N *= 2;
+			fprintf(fp, "Graph plot for %ldKB\n", (N/1024));
+			fflush(fp);
+			arr = (char*) malloc (N);
+			if (arr == NULL) {
+				printf("Unable to allocate memory\n");
+				exit(1);
+			}
+			strides = N / 2;
+			start = clock();
+			for (s = 0; s < strides; s++) {
+				arr[s] = (s * strides) + 1;
+			}
+			timeTaken = (double)(clock() - start)/CLOCKS_PER_SEC;
+			fprintf(fp, "Time for %.12f\n", timeTaken);
+			fflush(fp);
+			free(arr);
+			arr = NULL;
 		}
-		strides = ((sizeof (int)) * N ) / 2;
-		start = clock();
- 	   	for (s = 0; s < strides; i++) {
-    	    arr[i] = (s * strides) + 1;
-    	    sum += arr[i];
-    	}
-       	timeTaken = (double)(clock() - start)/CLOCKS_PER_SEC;
-    	fprintf(fp, "Time for %d: %.12f \n", sum, timeTaken);
-    	free(arr);
-    	arr = NULL;
 	}
-	
+
 	return 0;
 }
